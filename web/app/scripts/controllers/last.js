258b96d84589a6661b87447f672e85d91102603f
Flyers <contact@flyers-web.org>
2015-10-31 00:19:19 +0100

    Updated controller for infinite scroll management

--- web/app/scripts/controllers/last.js
+++ web/app/scripts/controllers/last.js
@@ -14,23 +14,29 @@ angular.module('bitCannonApp')
       'AngularJS',
       'Karma'
     ];
-    var init = function() {
-      $http.get($rootScope.api + 'last').
+    $scope.busy = false;
+    $scope.results = [];
+    $scope.infinite = function() {
+      if($scope.busy){return;}
+      $scope.busy = true;
+      $http.get($rootScope.api + 'last' + '/s/' + $scope.results.length).
         success(function(data, status) {
           if (status === 200) {
             for (var i = 0; i < data.length; i++) {
               var row = data[i];
               row.Details = '&tr='+row.Details.join('&tr=');
+              $scope.results.push(row);
             }
-            $scope.results = data;
+            if(data.length > 0) {
+              $scope.busy = false;
+            }
           }
-        else {
-          $rootScope.message = data.message;
-        }
+          else {
+            $rootScope.message = data.message;
+          }
         }).
         error(function() {
           $rootScope.message = 'API Request failed.';
         });
-    };
-    init();
+    }
   });
